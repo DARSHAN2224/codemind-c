a2018a8181f9494e5bf4cae41e8047f20655492b
DARSHAN P <126262121+DARSHAN2224@users.noreply.github.com>
2023-05-24 23:34:01 +0530

    #include<stdio.h> int main() {     int num,sum=0;     scanf("%d",&num);     int t=num;     while(num>0)     {         int rem=num%10;         sum=sum*10+rem;         num=num/10;     }     int temp=sum*sum,sum1=0;     // printf("%d ",temp);     while(temp>0)     {         int rem=temp%10;         sum1=sum1*10+rem;         temp=temp/10;     } //    printf("%d %d",sum1,num*num);
        if((t*t)==sum1)
        printf("True");
        else
        printf("False");
    }

---
+++ Adam_number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int num,sum=0;
+    scanf("%d",&num);
+    int t=num;
+    while(num>0)
+    {
+        int rem=num%10;
+        sum=sum*10+rem;
+        num=num/10;
+    }
+    int temp=sum*sum,sum1=0;
+    // printf("%d
+",temp);
+    while(temp>0)
+    {
+        int rem=temp%10;
+        sum1=sum1*10+rem;
+        temp=temp/10;
+    }
+//    printf("%d
+%d",sum1,num*num);
+    
+    if((t*t)==sum1)
+    printf("True");
+    else
+    printf("False");
+}
